dc4614ff2b388375f50aff4a8bb77a1ee329a995
Anton Briganti <antonbriganti.myob@gmail.com>
2019-05-22 15:45:02 +1000

    Added tests to check for status codes on success or fail

--- HelloWorldWebAppTests/PeopleControllerTest.cs
+++ HelloWorldWebAppTests/PeopleControllerTest.cs
@@ -45,6 +45,21 @@ namespace HelloWorldWebAppTests
         }
 
         [Fact]
+        public void AddPersonToWorld_Returns201_WhenNameDoesNotExistInRepository()
+        {
+            var testPerson = new Person("Anton");
+            var mockRepo = new Mock<IPeopleRepository>();
+            mockRepo.Setup(repo => repo.CheckIfNameExistsInRepository(testPerson.Name)).Returns(false);
+            mockRepo.Setup(repo => repo.AddPersonToRepository(testPerson));
+            var controller = new PeopleController(mockRepo.Object, new Mock<IMessageBuilder>().Object);
+
+            IActionResult result = controller.AddPersonToWorld(testPerson);
+            var goodRequest = result as CreatedAtActionResult;
+
+            Assert.Equal(201, goodRequest.StatusCode);
+        }
+
+        [Fact]
         public void AddPersonToWorld_DoesNotCall_AddPersonToRepository_WhenNameExists()
         {
             var testPerson = new Person("Anton");
@@ -59,14 +74,29 @@ namespace HelloWorldWebAppTests
         }
 
         [Fact]
-        public void AddPersonToWorld_ReturnsBadRequest_WhenNoPersonIsGiven()
+        public void AddPersonToWorld_Returns400_WhenNameExistsInRepository()
+        {
+            var testPerson = new Person("Anton");
+            var mockRepo = new Mock<IPeopleRepository>();
+            mockRepo.Setup(repo => repo.CheckIfNameExistsInRepository(testPerson.Name)).Returns(true);
+            var controller = new PeopleController(mockRepo.Object, new Mock<IMessageBuilder>().Object);
+
+            IActionResult result = controller.AddPersonToWorld(testPerson);
+            var badRequest = result as BadRequestObjectResult;
+
+            Assert.Equal(400, badRequest.StatusCode);
+        }
+
+        [Fact]
+        public void AddPersonToWorld_Returns400_WhenNoPersonIsGiven()
         {
             var controller =
                 new PeopleController(new Mock<IPeopleRepository>().Object, new Mock<IMessageBuilder>().Object);
 
-            var result = controller.AddPersonToWorld(null);
+            IActionResult result = controller.AddPersonToWorld(null);
+            var badRequest = result as BadRequestObjectResult;
 
-            Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal(400, badRequest.StatusCode);
         }
 
 
@@ -85,6 +115,21 @@ namespace HelloWorldWebAppTests
         }
 
         [Fact]
+        public void RemovePersonFromWorld_Returns200_WhenNameExists()
+        {
+            var testPerson = new Person("John");
+            var mockRepo = new Mock<IPeopleRepository>();
+            mockRepo.Setup(repo => repo.CheckIfNameExistsInRepository(testPerson.Name)).Returns(true);
+            mockRepo.Setup(repo => repo.RemovePersonFromRepository(testPerson));
+            var controller = new PeopleController(mockRepo.Object, new Mock<IMessageBuilder>().Object);
+
+            IActionResult result = controller.RemovePersonFromWorld(testPerson);
+            var goodRequest = result as OkObjectResult;
+
+            Assert.Equal(200, goodRequest.StatusCode);
+        }
+
+        [Fact]
         public void RemovePersonFromWorld_DoesNotCall_RemovePersonFromRepository_WhenNameDoesNotExists()
         {
             var testPerson = new Person("John");
@@ -99,6 +144,21 @@ namespace HelloWorldWebAppTests
         }
 
         [Fact]
+        public void RemovePersonFromWorld_Returns404_WhenNameDoesNotExists()
+        {
+            var testPerson = new Person("John");
+            var mockRepo = new Mock<IPeopleRepository>();
+            mockRepo.Setup(repo => repo.CheckIfNameExistsInRepository(testPerson.Name)).Returns(false);
+            mockRepo.Setup(repo => repo.RemovePersonFromRepository(testPerson));
+            var controller = new PeopleController(mockRepo.Object, new Mock<IMessageBuilder>().Object);
+
+            IActionResult result = controller.RemovePersonFromWorld(testPerson);
+            var badRequest = result as NotFoundObjectResult;
+
+            Assert.Equal(404, badRequest.StatusCode);
+        }
+
+        [Fact]
         public void RemovePersonFromWorld_DoesNotCall_RemovePersonFromRepository_WhenNameIsAnton()
         {
             var testPerson = new Person("Anton");
@@ -109,6 +169,21 @@ namespace HelloWorldWebAppTests
             controller.RemovePersonFromWorld(testPerson);
 
             mockRepo.Verify(repo => repo.RemovePersonFromRepository(testPerson), Times.Never);
+        }
+
+        [Fact]
+        public void RemovePersonFromWorld_Returns400_WhenNameIsAnton()
+        {
+            var testPerson = new Person("Anton");
+            var mockRepo = new Mock<IPeopleRepository>();
+            mockRepo.Setup(repo => repo.CheckIfNameExistsInRepository(testPerson.Name)).Returns(false);
+            mockRepo.Setup(repo => repo.RemovePersonFromRepository(testPerson));
+            var controller = new PeopleController(mockRepo.Object, new Mock<IMessageBuilder>().Object);
+
+            IActionResult result = controller.RemovePersonFromWorld(testPerson);
+            var badRequest = result as BadRequestObjectResult;
+
+            Assert.Equal(400, badRequest.StatusCode);
         }
 
         [Fact]
@@ -138,6 +213,22 @@ namespace HelloWorldWebAppTests
         }
 
         [Fact]
+        public void UpdatePersonInWorld_Returns200_WhenOldNameExistsInRepo_AndNewNameDoesNot()
+        {
+            var testRequest = new NameChangeRequest("Greg", "John");
+            var mockRepo = new Mock<IPeopleRepository>();
+            mockRepo.Setup(repo => repo.CheckIfNameExistsInRepository(testRequest.OldName)).Returns(true);
+            mockRepo.Setup(repo => repo.CheckIfNameExistsInRepository(testRequest.NewName)).Returns(false);
+            mockRepo.Setup(repo => repo.UpdatePersonInRepository(testRequest));
+            var controller = new PeopleController(mockRepo.Object, new Mock<IMessageBuilder>().Object);
+
+            IActionResult result = controller.UpdatePersonInWorld(testRequest);
+            var goodRequest = result as OkObjectResult;
+
+            Assert.Equal(200, goodRequest.StatusCode);
+        }
+
+        [Fact]
         public void UpdatePersonName_DoesNotCall_UpdatePersonInRepository_WhenNewNameDoesExistsInRepo()
         {
             var testRequest = new NameChangeRequest("Greg", "John");
@@ -150,6 +241,22 @@ namespace HelloWorldWebAppTests
             controller.UpdatePersonInWorld(testRequest);
 
             mockRepo.Verify(repo => repo.UpdatePersonInRepository(testRequest), Times.Never);
+        }
+
+        [Fact]
+        public void UpdatePersonName_Returns400_WhenNewNameDoesExistsInRepo()
+        {
+            var testRequest = new NameChangeRequest("Greg", "John");
+            var mockRepo = new Mock<IPeopleRepository>();
+            mockRepo.Setup(repo => repo.CheckIfNameExistsInRepository(testRequest.OldName)).Returns(false);
+            mockRepo.Setup(repo => repo.CheckIfNameExistsInRepository(testRequest.NewName)).Returns(true);
+            mockRepo.Setup(repo => repo.UpdatePersonInRepository(testRequest));
+            var controller = new PeopleController(mockRepo.Object, new Mock<IMessageBuilder>().Object);
+
+            IActionResult result = controller.UpdatePersonInWorld(testRequest);
+            var badRequest = result as BadRequestObjectResult;
+
+            Assert.Equal(400, badRequest.StatusCode);
         }
 
         [Fact]
@@ -168,6 +275,22 @@ namespace HelloWorldWebAppTests
         }
 
         [Fact]
+        public void UpdatePersonName_Returns400_WhenOldNameDoesNotExistInRepo()
+        {
+            var testRequest = new NameChangeRequest("Greg", "John");
+            var mockRepo = new Mock<IPeopleRepository>();
+            mockRepo.Setup(repo => repo.CheckIfNameExistsInRepository(testRequest.OldName)).Returns(false);
+            mockRepo.Setup(repo => repo.CheckIfNameExistsInRepository(testRequest.NewName)).Returns(false);
+            mockRepo.Setup(repo => repo.UpdatePersonInRepository(testRequest));
+            var controller = new PeopleController(mockRepo.Object, new Mock<IMessageBuilder>().Object);
+
+            IActionResult result = controller.UpdatePersonInWorld(testRequest);
+            var badRequest = result as BadRequestObjectResult;
+
+            Assert.Equal(400, badRequest.StatusCode);
+        }
+
+        [Fact]
         public void UpdatePersonName_DoesNotCall_UpdatePersonInRepository_WhenOldNameIsOwner()
         {
             var testRequest = new NameChangeRequest("Anton", "John");
@@ -178,6 +301,22 @@ namespace HelloWorldWebAppTests
             controller.UpdatePersonInWorld(testRequest);
 
             mockRepo.Verify(repo => repo.UpdatePersonInRepository(testRequest), Times.Never);
+        }
+
+        [Fact]
+        public void UpdatePersonName_Returns400_WhenOldNameIsOwner()
+        {
+            var testRequest = new NameChangeRequest("Anton", "John");
+            var mockRepo = new Mock<IPeopleRepository>();
+            mockRepo.Setup(repo => repo.UpdatePersonInRepository(testRequest));
+            var controller = new PeopleController(mockRepo.Object, new Mock<IMessageBuilder>().Object);
+
+            controller.UpdatePersonInWorld(testRequest);
+
+            IActionResult result = controller.UpdatePersonInWorld(testRequest);
+            var badRequest = result as BadRequestObjectResult;
+
+            Assert.Equal(400, badRequest.StatusCode);
         }
 
         [Fact]
